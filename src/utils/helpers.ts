d06dbfc57ac94c29e15a71e1679851a8461cd5df
Nika Kolesnikova <firstnamenika@gmail.com>
2024-08-16 18:41:39 -0400

    refactor sort function in helpers.ts

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -18,33 +18,21 @@ export const filterStatus = (todos: TodoInterface[], todoId: number) => {
       return updatedTodos;
 }
 
-export const sort:(todos: TodoInterface[], action: string) => Promise<TodoInterface[]> = async (todos, action) => {
-  let sortedTodos;
-  switch(action) {
-    case 'title true':
-      sortedTodos = [...todos].sort((a, b)=> a.body.localeCompare(b.body));
-      break;
-    case 'title false':
-      sortedTodos = [...todos].sort((a, b)=> b.body.localeCompare(a.body));
-      break;
-    case 'date true':
-      sortedTodos = [...todos].sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime());
-      break;
-    case 'date false':
-      sortedTodos = [...todos].sort((a, b) => new Date(a.date).getTime() - new Date(b.date).getTime());
-      break;
-    case 'check true':
-      sortedTodos = [...todos].sort((a, b) => Number(a.status) - Number(b.status));
-      break;
-    case 'check false':
-      sortedTodos = [...todos].sort((a, b) => Number(b.status) - Number(a.status));
-      break;
-    default:
-      sortedTodos = [...todos].sort((a, b) => new Date(a.date).getTime() - new Date(b.date).getTime());
-  }
-  console.log('are sorted todos an array? ', Array.isArray(sortedTodos));
+const sortingFunctions = {
+  'title true': (a, b) => a.body.localeCompare(b.body), // sort alphabetically in ascending order
+  'title false': (a, b) => b.body.localeCompare(a.body), // sort alphabetically in descending order
+  'date true': (a, b) => new Date(b.date).getTime() - new Date(a.date).getTime(), // sort by date, old first
+  'date false': (a, b) => new Date(a.date).getTime() - new Date(b.date).getTime(), // sort by date, new first
+  'check true': (a, b) => Number(a.status) - Number(b.status), // group all checked on top
+  'check false': (a, b) => Number(b.status) - Number(a.status), // group all checked on bottom
+}
+
+export const sort: (todos: TodoInterface[], action: string) => Promise<TodoInterface[]> = async (todos, action) => {
+  const sortingFunc = sortingFunctions[action] || sortingFunctions['date false']; // by default sort by date, new first 
+  const sortedTodos = [...todos].sort(sortingFunc);
   return sortedTodos;
 }
+
 
 export const filter = async (criterion: CriterionInterface) => {
   let filteredTodos;
