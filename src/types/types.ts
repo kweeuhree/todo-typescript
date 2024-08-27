d8101524bf032932788d55dce339dbb2f3770bb5
Nika Kolesnikova <firstnamenika@gmail.com>
2024-08-27 08:57:23 -0400

    add userAction that will be used in userReducer

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,10 +1,10 @@
-import { TodoInterface } from "../interfaces/interfaces";
+import { TodoInterface, UserInterface } from "../interfaces/interfaces";
 
 // Define a type for the keys of sortState
 export type SortStateKey = 'title' | 'date' | 'check';
 
-// Define a type for reducer actions
-export type Action =
+// Define a type for todo reducer actions
+export type TodoAction =
   | { type: 'INITIALIZE'; payload: TodoInterface[] }
   | { type: 'ERROR'; payload: string }
   | { type: 'TOGGLE_STATUS'; payload: string }
@@ -13,3 +13,13 @@ export type Action =
   | { type: 'CREATE'; payload: TodoInterface }
   | { type: 'UPDATE'; payload: TodoInterface }
   | { type: 'DELETE'; payload: TodoInterface };
+
+
+// Define a type for user reducer actions
+export type UserAction =
+  | { type: 'SIGNUP'; payload: UserInterface }
+  | { type: 'LOGIN'; payload: UserInterface }
+  | { type: 'LOGOUT'}
+  | { type: 'ERROR'; payload: string }
+  | { type: 'CSRF_TOKEN'; payload: string };
+  
